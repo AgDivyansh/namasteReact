6ec9325348ee239c328fc4cc6413a05c8d7b0981
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2025-07-28 11:11:35 +0530

    installing parcel and node modules

--- 1-Lecture/App.js
+++ 1-Lecture/App.js
@@ -1,12 +1,12 @@
- const first = React.createElement("div", {}, [
-            React.createElement("div", {id : "firstdiv"}, [
-                React.createElement("h1", {id : "first"}, "Aprilia Rs 457"),
-                React.createElement("h2" ,{ id : "second"}, "Ninja 500")
-            ]),
-            React.createElement("div", {di: "seconddiv"}, [
-                React.createElement("h1", {id: "third"}, "Ninja 400"),
-                React.createElement("h2", {id: "fourth"}, "Ninja 300")
-            ])
-        ]) ;
-        const root = ReactDOM.createRoot(document.getElementById("root")) ;
-        root.render(first)
+const first = React.createElement("div", {}, [
+    React.createElement("div", { id: "firstdiv" }, [
+        React.createElement("h1", { id: "first" }, "Aprilia Rs 457"),
+        React.createElement("h2", { id: "second" }, "Ninja 500")
+    ]),
+    React.createElement("div", { di: "seconddiv" }, [
+        React.createElement("h1", { id: "third" }, "Ninja 400"),
+        React.createElement("h2", { id: "fourth" }, "Ninja 300")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(first)
