db7eb3946e7a44fa1d4c74485df6439ec299db0e
AgDivyansh <93419706+AgDivyansh@users.noreply.github.com>
2025-07-28 23:52:29 +0530

    complete basics of npm and react

--- 1-Lecture/App.js
+++ 1-Lecture/App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const first = React.createElement("div", {}, [
-    React.createElement("div", { id: "firstdiv" }, [
-        React.createElement("h1", { id: "first" }, "Aprilia Rs 457"),
-        React.createElement("h2", { id: "second" }, "Ninja 500")
+    React.createElement("div", { id: "firstdiv", key : "firstdiv" }, [
+        React.createElement("h1", { id: "first", key: "Aprilia" }, "Aprilia Rs 457"),
+        React.createElement("h2", { id: "second", key: "Ninja500" }, "Ninja 500")
     ]),
-    React.createElement("div", { di: "seconddiv" }, [
-        React.createElement("h1", { id: "third" }, "Ninja 400"),
-        React.createElement("h2", { id: "fourth" }, "Ninja 300")
+    React.createElement("div", { id: "seconddiv", key : "seconddiv" }, [
+        React.createElement("h1", { id: "third", key: "Ninja400" }, "Ninja 400"),
+        React.createElement("h2", { id: "fourth", key: "Ninja300" }, "Ninja 300")
     ])
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
